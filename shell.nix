08b9f0c0ead505e2d0fb8acf0785ca1a8e2baba5
RyzeNGrind <14363544+RyzeNGrind@users.noreply.github.com>
2025-02-05 19:18:16 -0500

    Added pre-reqs for devin.cursorrules

--- shell.nix
+++ shell.nix
@@ -15,10 +15,13 @@ pkgs.mkShell {
     
     #github:grapeot/devin.cursorrules pre-reqs
     python314
+    python314Packages.pip
   ];
 
   shellHook = ''
     echo -e "\033[1;34mC Development Environment\033[0m"
-    echo -e "\033[1;32mAvailable tools: gcc, make, git, gdb, valgrind, python314\033[0m"
+    echo -e "\033[1;32mAvailable tools: gcc, make, git, gdb, valgrind, python, pip\033[0m"
+    echo -e "\033[1;32mPython version: $(python --version) \033[0m"
+    echo -e "\033[1;32mPip version: $(pip --version) \033[0m"
   '';
 }
