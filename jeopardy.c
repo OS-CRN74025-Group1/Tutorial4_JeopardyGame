a11b988932ec09beb1e81b8287fd943ccefe580c
js705 <jasleen.saini1@ontariotechu.net>
2025-02-06 19:31:22 -0500

    Update jeopardy.c

--- jeopardy.c
+++ jeopardy.c
@@ -37,18 +37,60 @@ int main(int argc, char *argv[])
     // Display the game introduction and initialize the questions
     initialize_game();
 
-    // Prompt for players names
-    
+    // Prompt for players names 
     // initialize each of the players in the array
+    for (int i=0; i<NUM_PLAYERS; i++){
+    	printf("Enter the name of player %d: ", i+1);
+    	fgets(players[i].name, MAX_LEN, stdin);
+    	players[i].name[strcspn(players[i].name, "\n")] = '\0';
+    	players[i].score = 0;
+    }
 
     // Perform an infinite loop getting command input from users until game ends
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
     {
         // Call functions from the questions and players source files
+        display_categories();
+        printf("Enter player name to choose a category: ");
+        fgets(buffer, BUFFER_LEN, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        
+        if (!player_exists(players, NUM_PLAYERS, buffer)){
+        	printf("Invalid player name\n");
+        	continue;
+        }
 
         // Execute the game until all questions are answered
-
-        // Display the final results and exit
+        char category[MAX_LEN];
+        int value;
+        printf("Enter category and value: ");
+        scanf("%s %d", category, &value);
+        getchar();
+        
+        if (already_answered(category, value)){
+        	printf("Question already answered. Choose another.\n");
+        	continue;
+        }
+        
+        display_question(category, value);
+        printf("Enter your answer: ");
+        fgets(buffer, BUFFER_LEN, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        char tokenized_answer[MAX_LEN] = "";
+        tokenize(buffer, &tokenized_answer);
+        
+        if (valid_answer(category, value, tokenized_answer)){
+        	printf("Correct answer!\n");
+        	update_score(players, NUM_PLAYERS, players[i].name, value);
+        }
+        else{
+        	printf("Incorrect! The correct answer was %s\n: ", questions[i].answer);
+        }
+        already_answered(category, value);
     }
+    
+    // Display the final results and exit
+    show_results(players, NUM_PLAYERS);
+    
     return EXIT_SUCCESS;
 }
